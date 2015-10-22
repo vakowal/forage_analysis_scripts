054a055e06242a4c79810db7d8740e3b86370c10
Ginger Kowal <gkowal@stanford.edu>
2015-10-22 11:01:51 -0400

    It works!

--- Test_empirical_stocking_density_OPC.py
+++ Test_empirical_stocking_density_OPC.py
@@ -17,16 +17,32 @@ def run_simulations():
     century_dir = 'C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/Century46_PC_Jan-2014'
     fix_file = 'drytrpfi.100'
     graz_file = os.path.join(century_dir, "graz.100")
-    site_list = ['kamok', 'loidien', 'research', 'rongai']
+    site_list = ['Kamok', 'Loidien', 'Research', 'Rongai']
     input_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/Kenya/input"
     outer_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/Output/Stocking_density_test"
     prop_legume = 0
     template_level = 'GL'
-
+    herb_class_weights = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/Kenya/Boran_weights.csv"
+    sd_dir = 'C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/Kenya/OPC_stocking_density'
+    breed = 'Boran'
+    steepness = 1.
+    latitude = 0
+    supp_available = 0
+    FParam = FreerParam.FreerParam(forage.get_general_breed(breed))
+    supp = forage.Supplement(FParam, 0, 0, 0, 0, 0, 0)
+    forage.set_time_step('month')
+    add_event = 1
+    
     grass_file = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/model_inputs/grass.csv"
-    grass = (pandas.read_csv(args[u'grass_csv'])).to_dict(orient='records')[0]
+    grass = (pandas.read_csv(grass_file)).to_dict(orient='records')[0]
 
     for site in site_list:
+        spin_up_outputs = [site+'_hist_log.txt', site+'_hist.lis']
+        century_outputs = [site+'_log.txt', site+'.lis', site+'.bin']
+        filename = 'average_animals_%s_2km_per_ha.csv' % site
+        stocking_density_file = os.path.join(sd_dir, filename)
+        sd_df = pandas.read_table(stocking_density_file, sep=',')
+        
         outdir = os.path.join(outer_dir, site)
         if not os.path.exists(outdir):
             os.makedirs(outdir)
@@ -41,37 +57,58 @@ def run_simulations():
         schedule = site + '.sch'
         output = site
         extend = site + '_hist'
-        cent.write_century_bat(args[u'century_dir'], extend_bat, schedule,
+        cent.write_century_bat(century_dir, extend_bat, schedule,
                                output, fix_file, 'outvars.txt', extend)
-        # move batch files to century dir
-        schedule = os.path.join(input_dir, site + '.sch')
-        files_to_move = [hist_bat, extend_bat, schedule]
-        file_list = [os.path.join(input_dir, f) for f in files_to_move]
+        # move CENTURY run files to CENTURY dir
+        site_file = os.path.join(input_dir, site + '.100')
+        weather_file = os.path.join(input_dir, site + '.wth')
+        e_schedule = os.path.join(input_dir, site + '.sch')
+        h_schedule = os.path.join(input_dir, site + '_hist.sch')
+        file_list = [hist_bat, extend_bat, e_schedule, h_schedule, site_file,
+                     weather_file]
         for file in file_list:
-            shutil.copyfile(file, os.path.join(century_dir, os.path.basename(file)))
+            shutil.copyfile(file, os.path.join(century_dir,
+                                               os.path.basename(file)))
         
         # make a copy of the original graz params and schedule file
-        shutil.copyfile(graz_file, os.path.join(args[u'century_dir'],
-                        'graz_orig.100'))
-        label = os.path.basename(schedule)[:-4]
+        shutil.copyfile(graz_file, os.path.join(century_dir, 'graz_orig.100'))
+        label = os.path.basename(e_schedule)[:-4]
         copy_name = label + '_orig.sch'
-        shutil.copyfile(schedule, os.path.join(input_dir, copy_name))
+        shutil.copyfile(e_schedule, os.path.join(input_dir, copy_name))
         
         # run CENTURY for spin-up up to start_year and start_month
-        hist_bat = os.path.join(args[u'century_dir'], site + '_hist.bat')
-        century_bat = os.path.join(args[u'century_dir'], site + '.bat')
-        p = Popen(["cmd.exe", "/c " + hist_bat], cwd=args[u'century_dir'])
+        hist_bat = os.path.join(century_dir, site + '_hist.bat')
+        century_bat = os.path.join(century_dir, site + '.bat')
+        p = Popen(["cmd.exe", "/c " + hist_bat], cwd=century_dir)
         stdout, stderr = p.communicate()
-        p = Popen(["cmd.exe", "/c " + century_bat], cwd=args[u'century_dir'])
+        p = Popen(["cmd.exe", "/c " + century_bat], cwd=century_dir)
         stdout, stderr = p.communicate()
         
+        # save copies of CENTURY outputs, but remove from CENTURY dir
+        intermediate_dir = os.path.join(outdir, 'CENTURY_outputs_spin_up')
+        if not os.path.exists(intermediate_dir):
+            os.makedirs(intermediate_dir)
+        to_move = century_outputs + spin_up_outputs
+        for file in to_move:
+            shutil.copyfile(os.path.join(century_dir, file), os.path.join(
+                            intermediate_dir, file))
+            os.remove(os.path.join(century_dir, file))
+        
         grass_list = [grass]
-        # stocking_density_file = ?
-        sd_df = pandas.read_table(stocking_density_file, sep=',')
+        herbivore_input = (pandas.read_csv(herb_class_weights).to_dict(
+                           orient='records'))
+        herbivore_list = []
+        for h_class in herbivore_input:
+            herd = forage.HerbivoreClass(FParam, breed,
+                                         h_class['weight'],
+                                         h_class['sex'], h_class['age'],
+                                         h_class['stocking_density'],
+                                         h_class['label'])
+            herd.update(FParam, 0, 0)
+            herbivore_list.append(herd)
         results_dict = {'year': [], 'month': []}
-        for h_class in ['Bulls', 'Cows', 'Calves', 'Heifers', 'Steers', 'Weaners',
-                        'steer/heifer']:
-            results_dict[h_class + '_gain_kg'] = []
+        for h_class in herbivore_list:
+            results_dict[h_class.label + '_gain_kg'] = []
         results_dict['milk_prod_kg'] = []
         for grass in grass_list:
             results_dict[grass['label'] + '_green_kgha'] = []
@@ -79,15 +116,30 @@ def run_simulations():
         
         try:
             for row in xrange(len(sd_df)):
+                herbivore_list = []
+                for h_class in herbivore_input:
+                    herd = forage.HerbivoreClass(FParam, breed,
+                                                 h_class['weight'],
+                                                 h_class['sex'], h_class['age'],
+                                                 h_class['stocking_density'],
+                                                 h_class['label'])
+                    herd.update(FParam, 0, 0)
+                    herbivore_list.append(herd)
+                for h_class in herbivore_list:
+                    density = sd_df.iloc[row][h_class.label]
+                    if density == 0:
+                        herbivore_list.remove(h_class)
+                    else:
+                        h_class.stocking_density = density
                 # TODO populate herbivore_list from sd_df
                 total_SD = forage.calc_total_stocking_density(herbivore_list)
-                site = forage.SiteInfo(total_SD, args[u'steepness'],
-                                       args[u'latitude'])
+                siteinfo = forage.SiteInfo(total_SD, steepness, latitude)
                 month = sd_df.iloc[row].month
-                year = sd.df.iloc[row].year
+                year = sd_df.iloc[row].year
+                suf = '%d-%d' % (month, year)
                 DOY = month * 30
                 # get biomass and crude protein for each grass type from CENTURY
-                output_file = os.path.join(args[u'century_dir'], site + '.lis')
+                output_file = os.path.join(intermediate_dir, site + '.lis')
                 outputs = cent.read_CENTURY_outputs(output_file, year, year + 2)
                 target_month = cent.find_prev_month(year, month)
                 grass['prev_g_gm2'] = grass['green_gm2']
@@ -104,14 +156,15 @@ def run_simulations():
                     available_forage = forage.calc_feed_types(grass_list)
                 else:
                     available_forage = forage.update_feed_types(grass_list,
-                                                                available_forage)
+                                                              available_forage)
                 results_dict['year'].append(year)
                 results_dict['month'].append(month)
                 for feed_type in available_forage:
-                    results_dict[feed_type.label + '_' + feed_type.green_or_dead +
+                    results_dict[feed_type.label + '_' +
+                                 feed_type.green_or_dead +
                                  '_kgha'].append(feed_type.biomass)
 
-                site.calc_distance_walked(FParam, available_forage)
+                siteinfo.calc_distance_walked(FParam, available_forage)
                 for feed_type in available_forage:
                     feed_type.calc_digestibility_from_protein()
 
@@ -137,7 +190,7 @@ def run_simulations():
                                                     max_intake, FParam,
                                                     available_forage)
                     diet_interm = forage.calc_diet_intermediates(FParam, diet,
-                                    supp, herb_class, site, prop_legume,
+                                    supp, herb_class, siteinfo, prop_legume,
                                     DOY)
                     reduced_max_intake = forage.check_max_intake(FParam, diet,
                                                 diet_interm, herb_class,
@@ -148,7 +201,7 @@ def run_simulations():
                                                         reduced_max_intake, FParam,
                                                         available_forage)
                         diet_interm = forage.calc_diet_intermediates(FParam, diet,
-                                        supp, herb_class, site,
+                                        supp, herb_class, siteinfo,
                                         prop_legume, DOY)
                     if herb_class.sex == 'lac_female':
                         milk_production = forage.check_milk_production(FParam,
@@ -178,32 +231,46 @@ def run_simulations():
                 schedule = os.path.join(century_dir, site + '.sch')
                 target_dict = cent.find_target_month(add_event, schedule, date,
                                                      1)
+                if target_dict == 0:
+                    er = "Error: no opportunities exist to add grazing event"
+                    raise Exception(er)
                 new_code = cent.add_new_graz_level(grass, total_consumed,
                                                    graz_file,
                                                    template_level,
-                                                   outdir, step)
+                                                   outdir, suf)
                 cent.modify_schedule(schedule, add_event, target_dict,
-                                     new_code, outdir, step)
+                                     new_code, outdir, suf)
 
                 # call CENTURY from the batch file
                 century_bat = os.path.join(century_dir, site + '.bat')
                 p = Popen(["cmd.exe", "/c " + century_bat],
-                          cwd=args[u'century_dir'])
+                          cwd=century_dir)
                 stdout, stderr = p.communicate()
+                # save copies of CENTURY outputs, but remove from CENTURY dir
+                intermediate_dir = os.path.join(outdir,
+                                     'CENTURY_outputs_m%d_y%d' % (month, year))
+                if not os.path.exists(intermediate_dir):
+                    os.makedirs(intermediate_dir)
+                for file in century_outputs:
+                    shutil.copyfile(os.path.join(century_dir, file),
+                                    os.path.join(intermediate_dir, file))
+                    os.remove(os.path.join(century_dir, file))
 
         # remove files from CENTURY directory
         finally:
             # replace graz params used by CENTURY with original file
             os.remove(graz_file)
-            shutil.copyfile(os.path.join(args[u'century_dir'], 'graz_orig.100'),
+            shutil.copyfile(os.path.join(century_dir, 'graz_orig.100'),
                             graz_file)
-            os.remove(os.path.join(args[u'century_dir'], 'graz_orig.100'))
-            files_to_remove = [os.path.join(century_dir, f) for f in files_to_move]
+            os.remove(os.path.join(century_dir, 'graz_orig.100'))
+            files_to_remove = [os.path.join(century_dir, os.path.basename(
+                                                       f)) for f in file_list]
             for file in files_to_remove:
                 os.remove(file)
-            # for grass in grass_list:
-                # os.remove(??)
-            os.remove(output_file)    
+            os.remove(os.path.join(century_dir, site + '_hist.bin'))
+            filled_dict = forage.fill_dict(results_dict, 'NA')
+            df = pandas.DataFrame(filled_dict)
+            df.to_csv(os.path.join(outdir, 'summary_results.csv'))
 
 def calculate_density(in_folder, result_dir):
     """Convert counts of animals within a certain distance of a point to
@@ -234,5 +301,6 @@ def calculate_density(in_folder, result_dir):
 
 if __name__ == "__main__":
     in_folder = 'C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Data/Kenya/From_Sharon/From_Sharon_5.29.15/Matched_GPS_records/Matched_with_weather_stations'
-    result_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/CENTURY_runs_Kenya/OPC_stocking_density"
-    calculate_density(in_folder, result_dir)
+    result_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/CENTURY4.6/Kenya/OPC_stocking_density"
+    # calculate_density(in_folder, result_dir)
+    run_simulations()
