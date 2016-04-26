d4b98337c1c134aaab0f2286b85b3fe2bbbe46ab
Ginger Kowal <ginger.kowal@gmail.com>
2016-04-26 18:11:19 -0400

    new launch structure to do runs to integrate with SWAT

--- CGIAR_launch_forage.py
+++ CGIAR_launch_forage.py
@@ -5,13 +5,12 @@ import forage
 
 if __name__ == "__main__":
     input_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Forage_model_inputs"
-    outer_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Forage_model_results/raw_3.15.16"
+    outer_dir = "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Forage_model_results/raw_4.26.16"
     forage_args = {
         'latitude': -12.55,
         'prop_legume': 0.0,
         'steepness': 1.,
         'DOY': 1,
-        'start_year': 2015,
         'start_month': 1,
         'num_months': 12,
         'mgmt_threshold': 0.1,
@@ -26,19 +25,20 @@ if __name__ == "__main__":
         'supp_csv': "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/model_inputs/Rubanza_et_al_2005_supp.csv",
         'input_dir': input_dir,
     }
-    for soil_zone in [0, 1, 2]:
-        for clim_zone in [0, 1, 2]:
-            for anim_type in ['cow', 'sheep', 'camelid']:
-                for sd_level in ['low', 'med', 'high']:
-                    grass_csv = os.path.join(input_dir, 'Pajonal_%d_%d.csv' %
-                                             (soil_zone, clim_zone))
-                    herbivore_csv = os.path.join(input_dir, anim_type + '_' +
-                                                 sd_level + '_sd.csv')
-                    out_dir = os.path.join(outer_dir, 's%d_c%d_%s_%s' %
-                                           (soil_zone, clim_zone, anim_type,
-                                           sd_level))
-                    forage_args['grass_csv'] = grass_csv
-                    forage_args['herbivore_csv'] = herbivore_csv
+    for subbasin in range(1, 15):
+        for anim_type in ['cow', 'sheep', 'camelid']:
+            for sd_level in ['low', 'med', 'high']:
+                grass_csv = os.path.join(input_dir, 'Pajonal_%d.csv' %
+                                         subbasin)
+                herbivore_csv = os.path.join(input_dir, anim_type + '_' +
+                                             sd_level + '_sd.csv')
+                forage_args['grass_csv'] = grass_csv
+                forage_args['herbivore_csv'] = herbivore_csv
+                for year in range(1991, 2010):
+                    forage_args['start_year'] = year
+                    out_dir = os.path.join(outer_dir, 's%d_%s_%s_%s' %
+                                           (subbasin, anim_type, sd_level, 
+                                            year))
                     forage_args['outdir'] = out_dir
-                    if not os.path.exists(out_dir):
-                        forage.execute(forage_args)
+                    # if not os.path.exists(out_dir):
+                    forage.execute(forage_args)
