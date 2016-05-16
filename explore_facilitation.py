3c86f38965d14ac35f3e39674ebced80e00644a7
Ginger Kowal <ginger.kowal@gmail.com>
2016-05-15 20:12:56 -0400

    fleshed out facilitation testing structure.

--- explore_facilitation.py
+++ explore_facilitation.py
@@ -5,18 +5,40 @@ import os
 import shutil
 import pandas
 import math
+import numpy as np
 from operator import attrgetter
 import sys
 sys.path.append('C:/Users/Ginger/Documents/Python/invest_forage_dev/src/natcap/invest/forage')
 import forage_utils as forage
 import freer_param as FreerParam
 
-def write_grass_csv(csv, abun_mean, cp_mean, abun_ratio, cp_ratio):
-    """Write a csv describing two grass types that relate to each other as
-    described by the abundance ratio and the crude protein ratio."""
+def fabricate_forage(total_biomass, abun_ratio, cp_mean, cp_ratio,
+                     abun_cp_same):
+    """Populate available forage with two grass types that relate to each other
+    as described by the abundance ratio (larger:smaller) and the crude protein
+    ratio (larger:smaller).  If abun_cp_same is true, the grass with higher
+    abundance also has higher crude protein."""
     
-    df = pandas.read_csv(csv)
+    lower_abun = float(total_biomass) / (abun_ratio + 1.)
+    higher_abun = float(total_biomass) - lower_abun
+    lower_cp = (2. * cp_mean) / (cp_ratio + 1.)
+    higher_cp = float(cp_ratio) * lower_cp
     
+    available_forage = []
+    if abun_cp_same:
+        available_forage.append(forage.FeedType('grass1', 'green', lower_abun,
+                                0., lower_cp, 'C4'))
+        available_forage.append(forage.FeedType('grass2', 'green', higher_abun,
+                                0., higher_cp, 'C4'))
+    else:
+        available_forage.append(forage.FeedType('grass1', 'green', lower_abun,
+                                0., higher_cp, 'C4'))
+        available_forage.append(forage.FeedType('grass2', 'green', higher_abun,
+                                0., lower_cp, 'C4'))
+    for feed_type in available_forage:
+        feed_type.rel_availability = feed_type.biomass / total_biomass
+        feed_type.calc_digestibility_from_protein()
+    return available_forage
 
 def diet_selection_t2(ZF, HR, prop_legume, supp_available, supp, Imax, FParam,
                       available_forage, idx, f_w=0, q_w=0, force_supp=None):
@@ -85,8 +107,9 @@ def diet_selection_t2(ZF, HR, prop_legume, supp_available, supp, Imax, FParam,
         # weight proportional intake by quantity weight and quality weight
         R_w.append(R[f_index] + F[f_index]*f_w + RQ[f_index]*q_w)
     # rescale weighted proportions to retain original sum of R
+    sum_Rw = sum(R_w)
     for f_index in range(len(available_forage)):
-        R_w[f_index] = (R_w[f_index] / sum(R_w)) * sum(R)
+        R_w[f_index] = (R_w[f_index] / sum_Rw) * sum(R)
     for f_index in range(len(available_forage)):    
         I.append(Imax * R_w[f_index])  # eq 27
         diet_selected.DMDf += (I[f_index] *
@@ -99,24 +122,17 @@ def diet_selection_t2(ZF, HR, prop_legume, supp_available, supp, Imax, FParam,
         f_label = available_forage[f_index].label + ';' +\
                   available_forage[f_index].green_or_dead
         diet_selected.intake[f_label] = I[f_index]
-    print "diet %d: " % idx
-    print "quality weight: %f, availability weight: %f" % (q_w, f_w)
-    print "F: "
-    print F
-    print "RQ: "
-    print RQ
-    print "R: "
-    print R
-    print "R weighted: "
-    print R_w
+
     diet_selected.DMDf = diet_selected.DMDf / diet_selected.If
     if supp_selected:
         Rs = Fs * supp.RQ  # eq 25
         diet_selected.Is = Imax * Rs  # eq 30
     return diet_selected
 
-def different_diets():
-    """Create different diets for two hypothetical animal types."""
+def different_diets(available_forage, herbivore_csv, weight_1):
+    """Create different diets for two hypothetical animal types.  The weight
+    ratio describes the ratio of """
+    
     time_step = 'month'
     forage.set_time_step(time_step)
     total_SD = 2
@@ -134,12 +150,6 @@ def different_diets():
                              supp_info['ether_extract'],
                              supp_info['crude_protein'],
                              supp_info['rumen_degradability'])
-                             
-    grass_csv = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/model_inputs/grasses_diet_illustration.csv"
-    grass_list = (pandas.read_csv(grass_csv)).to_dict(orient='records')
-    available_forage = forage.calc_feed_types(grass_list)
-    
-    herbivore_csv = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/model_inputs/herbs_diet_illustration.csv"
     herbivore_input = (pandas.read_csv(herbivore_csv)).to_dict(orient='records')
     herbivore_list = []
     for h_class in herbivore_input:
@@ -147,7 +157,7 @@ def different_diets():
         herd.update()
         herbivore_list.append(herd)
     
-    avail_weights = [0.1, 0.9]
+    avail_weights = [weight_1, 1 - weight_1]
     qual_weights = [1.0 - w for w in avail_weights]
     
     diet_dict = {}        
@@ -180,6 +190,7 @@ def different_diets():
                                                 # herb_class.FParam,
                                                 # available_forage)
         diet_dict[herb_class.label] = diet
+    return diet_dict
     # forage.reduce_demand(diet_dict, stocking_density_dict,
                          # available_forage)
     diet_interm = forage.calc_diet_intermediates(
@@ -190,5 +201,68 @@ def different_diets():
     herb_class.update(delta_weight=delta_W_step,
                       delta_time=forage.find_days_per_step())
 
+def calc_diet_segregation(diet_dict):
+    """Calculate the segregation between diets of two herbivores. This is
+    calculated as the average difference in percentage of the diet comprised of
+    each forage type.  So a value of 1 means they eat completely separate grass
+    types. A value of 0 means they select grass types in identical
+    proportions."""
+    
+    perc_lists = []
+    for hclass_label in diet_dict.keys():
+        percent_consumed = []
+        total_consumed = 0.
+        for grass_label in diet_dict[hclass_label].intake.keys():
+            total_consumed = total_consumed + \
+                                    diet_dict[hclass_label].intake[grass_label]
+        for grass_label in diet_dict[hclass_label].intake.keys():
+            percent_consumed.append(
+                    diet_dict[hclass_label].intake[grass_label]/total_consumed)
+        perc_lists.append(percent_consumed)
+    assert len(perc_lists) == 2
+    assert len(perc_lists[0]) == len(perc_lists[1])
+    diff_list = [abs(i - j) for i, j in zip(perc_lists[0], perc_lists[1])]
+    ave_diff = sum(diff_list) / len(diff_list)
+    return ave_diff                        
+    
+def run_test(save_as):
+    """Change forage characteristics, change herbivore characteristics,
+    measure outcomes."""
+    
+    summary_dict = {'total_biomass': [],
+                    'abundance_ratio': [],
+                    'cp_mean': [],
+                    'cp_ratio': [],
+                    'abundance_cp_same': [],
+                    'weight_1': [],
+                    'segregation': [],
+                    }
+    herbivore_csv = "C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/model_inputs/herbs_diet_illustration.csv"
+    for total_biomass in np.linspace(40, 4000, 10):
+        for abun_ratio in np.linspace(1.5, 10, 10):
+            for cp_mean in np.linspace(0.04, 0.2, 10):
+                for cp_ratio in np.linspace(1.5, 10, 10):
+                    for abun_cp_same in [0, 1]:
+                        for weight_1 in np.linspace(0.01, 0.45, 10):
+                            available_forage = fabricate_forage(
+                                                 total_biomass, abun_ratio,
+                                                 cp_mean, cp_ratio,
+                                                 abun_cp_same)
+                            diet_dict = different_diets(
+                                                 available_forage,
+                                                 herbivore_csv, weight_1)
+                            segregation = calc_diet_segregation(diet_dict)
+                            summary_dict['total_biomass'].append(total_biomass)
+                            summary_dict['abundance_ratio'].append(abun_ratio)
+                            summary_dict['cp_mean'].append(cp_mean)
+                            summary_dict['cp_ratio'].append(cp_ratio)
+                            summary_dict['abundance_cp_same'].append(
+                                                                  abun_cp_same)
+                            summary_dict['weight_1'].append(weight_1)
+                            summary_dict['segregation'].append(segregation)
+    df = pandas.DataFrame(summary_dict)
+    df.to_csv(save_as)
+    
 if __name__ == "__main__":
-    different_diets()
+    save_as = 'C:/Users/Ginger/Dropbox/NatCap_backup/Forage_model/Forage_model/facilitation_exploration/test_segregation.csv'
+    run_test(save_as)
