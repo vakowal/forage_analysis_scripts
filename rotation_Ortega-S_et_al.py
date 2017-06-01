3dca00be0b62f0d24e4ce541463d9a3aad44637d
Ginger Kowal <ginger.kowal@gmail.com>
2017-06-01 14:52:17 -0400

    latest changes for analyzing effects of rotation with Ortega-S case study

--- rotation_Ortega-S_et_al.py
+++ rotation_Ortega-S_et_al.py
@@ -99,7 +99,20 @@ def stocking_dens_test_wrapper(outer_outdir):
     result_df = pd.DataFrame(result_dict)
     result_df.to_csv(os.path.join(outer_outdir, 'summary.csv'))
 
-def get_biomass(outdir, date):
+def get_biomass_from_summary_results(outdir, step):
+    """Retrieve bimoass from summary results file on the specified step of the
+    model."""
+    
+    grass_label = 'grass'  # TODO dynamic
+    sum_df = pd.read_csv(os.path.join(outdir, 'summary_results.csv'))
+    sum_df.set_index(['step'], inplace=True)
+    live_kgha = sum_df.get_value(step, '{}_green_kgha'.format(grass_label))
+    dead_kgha = sum_df.get_value(step, '{}_dead_kgha'.format(grass_label))
+    live_gm2 = live_kgha / 10.
+    dead_gm2 = dead_kgha / 10.
+    return live_gm2, dead_gm2
+   
+def get_biomass_from_Century_outputs(outdir, date):
     """Retrieve biomass from model outputs on the specified date. Date must be
     in "Century format", e.g. 2012.00 is December of 2011."""
     
@@ -144,12 +157,12 @@ def get_date(forage_args, step):
     date = year + float('%.2f' % (month / 12.))
     return date
 
-def diet_selection(forage_args, live_gm2, dead_gm2, defoliation_step):
+def diet_selection(forage_args, live_gm2, dead_gm2):
     """Perform diet selection on forage, as a metric of forage quality. Return
-    diet selected by an individual animal from the forage available on the date
-    defoliation_step.  defoliation_step should be supplied relative to model
-    steps, e.g. step 0 is first step of the model run, or
-    forage_args['start_year'], forage_args['start_month']"""
+    diet selected by an individual animal from the forage available as
+    described by live_gm2 and dead_gm2.  Assume that digestibility of forage
+    is calculated from crude protein, and crude protein is specified in the
+    grass csv (i.e. fixed)."""
     
     forage_u.set_time_step('month')
     herbivore_list = []
@@ -167,7 +180,8 @@ def diet_selection(forage_args, live_gm2, dead_gm2, defoliation_step):
     grass_list[0]['dead_gm2'] = dead_gm2
     available_forage = forage_u.calc_feed_types(grass_list)
     for feed_type in available_forage:
-        feed_type.calc_digestibility_from_protein()  # TODO n_mult??
+        feed_type.calc_digestibility_from_protein(
+                                             forage_args['digestibility_flag'])  # TODO n_mult??
     assert len(herbivore_list) == 1, "must be one herbivore type"
     herb_class = herbivore_list[0]
     herb_class.calc_distance_walked(1., total_SD, available_forage)
@@ -193,50 +207,121 @@ def diet_selection(forage_args, live_gm2, dead_gm2, defoliation_step):
                                                 
 def optimal_defoliation_test():
     """Test grass response to different grazing levels explicitly.  Confirm
-    that optimal defolation level for biomass regrowth is 25%"""
+    that optimal defolation level for biomass regrowth is 25%.  Impose
+    specific defoliation levels (e.g. 25% defoliation) on a particular date,
+    specified as a model step."""
     
     outer_outdir = r"C:\Users\Ginger\Dropbox\NatCap_backup\Forage_model\Forage_model\model_results\WitW\Ortega-S_et_al\defol_exp"
     forage_args = default_forage_args()
     
-    result_dict = {'defoliation_step': [], 'defoliation_level': [],
-                   'live_biomass_prior_to_defol': [],
-                   'live_biomass_next_step': []}
-    for defol_step in [6]:  # start with one month
-        # target_month is specified relative to steps, so grz_months
+    recovery_dict = {'defoliation_step': [], 'defoliation_level': [],
+                     'steps_to_recover': [], 'stocking_density': []}
+    perc_regrowth_dict = {'defoliation_step': [], 'defoliation_level': [],
+                     'perc_regrowth_defol_step': [], 'perc_regrowth_d+1': [],
+                     'live_gm2_removed': []}
+    growth_dict = {'defoliation_step': [], 'defoliation_level': [],
+                     'growth_defol_step': [], 'growth_d+1': [],
+                     'live_gm2_removed': []}
+
+    for defol_step in [2, 4, 6, 8, 10, 12]:
+        record_biomass_step = defol_step - 1
         forage_args['grz_months'] = []
         forage_args['outdir'] = os.path.join(outer_outdir, 'zero_sd')
         if not os.path.exists(os.path.join(forage_args['outdir'],
                                            'summary_results.csv')):
             forage.execute(forage_args)
-        defol_date = get_date(forage_args, defol_step)
-        live_gm2, dead_gm2 = get_biomass(forage_args['outdir'], defol_date)
+        live_gm2, dead_gm2 = get_biomass_from_summary_results(
+                                    forage_args['outdir'], record_biomass_step)
         
         # calculate diet selected in defoliation month: then can change stocking density to 
         # remove specified amount of biomass
-        diet = diet_selection(forage_args, live_gm2, dead_gm2, defol_step)  # daily intake
+        diet = diet_selection(forage_args, live_gm2, dead_gm2)  # daily intake
         monthly_intake_live = forage_u.convert_daily_to_step(diet.intake['grass;green'])
         monthly_intake_live_gm2 = monthly_intake_live / 10.
-        for defol_level in [0.1, 0.15]:  # , 0.2, 0.25, 0.3, 0.35]:
+        for defol_level in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]:
             # for now, calculate stocking dens based on % live biomass consumed
             live_gm2_consumed = defol_level * live_gm2
             stocking_dens = live_gm2_consumed / monthly_intake_live_gm2
             rotation.modify_stocking_density(forage_args['herbivore_csv'],
                                              stocking_dens)
             outdir = os.path.join(outer_outdir,
-                                  'defol_{0:.2f}'.format(defol_level))
+                                  'defol_{}_m{}'.format(round(defol_level, 2),
+                                                        defol_step))
             forage_args['outdir'] = outdir
             forage_args['grz_months'] = [defol_step]
-            forage.execute(forage_args)
-            regrowth_step = defol_step + 1
-            regrowth_date = get_date(forage_args, regrowth_step)
-            # record biomass in following step
-            live_gm2_after, dead_gm2_after = get_biomass(outdir, defol_date)
-            result_dict['defoliation_step'].append(defol_step)
-            result_dict['defoliation_level'].append(defol_level)
-            result_dict['live_biomass_prior_to_defol'].append(live_gm2)
-            result_dict['live_biomass_next_step'].append(live_gm2_after)
-    result_df = pd.DataFrame(result_dict)
-    result_df.to_csv(os.path.join(outer_outdir, 'regrowth_summary.csv'))
-    
+            if not os.path.exists(os.path.join(forage_args['outdir'],
+                                  'summary_results.csv')):
+                forage.execute(forage_args)
+            i = 0
+            biomass = 0
+            while biomass < live_gm2:
+                regrowth_step = defol_step + i
+                try:
+                    biomass, dead_biomass = get_biomass_from_summary_results(
+                                                         outdir, regrowth_step)
+                except:
+                    regrowth_step = defol_step + 999
+                    break
+                i += 1
+            steps_to_recover = regrowth_step - defol_step
+            recovery_dict['defoliation_step'].append(defol_step)
+            recovery_dict['defoliation_level'].append(defol_level)
+            recovery_dict['steps_to_recover'].append(steps_to_recover)
+            recovery_dict['stocking_density'].append(stocking_dens)
+            
+            defoliated = live_gm2 - live_gm2_consumed
+            defol_live, defol_dead = get_biomass_from_summary_results(
+                                                            outdir, defol_step)
+            d1_step = defol_step + 1
+            dplus1_live, dplus1_dead = get_biomass_from_summary_results(
+                                                               outdir, d1_step)
+            regrowth_defol = (defol_live - defoliated) / defoliated
+            regrowth_d1 = (dplus1_live - defol_live) / defol_live
+            growth_defol = defol_live - defoliated
+            growth_d1 = dplus1_live - defol_live
+            perc_regrowth_dict['defoliation_step'].append(defol_step)
+            perc_regrowth_dict['defoliation_level'].append(defol_level)
+            perc_regrowth_dict['perc_regrowth_defol_step'].append(regrowth_defol)
+            perc_regrowth_dict['perc_regrowth_d+1'].append(regrowth_d1)
+            perc_regrowth_dict['live_gm2_removed'].append(live_gm2_consumed)
+            
+            growth_dict['defoliation_step'].append(defol_step)
+            growth_dict['defoliation_level'].append(defol_level)
+            growth_dict['growth_defol_step'].append(growth_defol)
+            growth_dict['growth_d+1'].append(growth_d1)
+            growth_dict['live_gm2_removed'].append(live_gm2_consumed)
+            
+    recovery_df = pd.DataFrame(recovery_dict)
+    recovery_df.to_csv(os.path.join(outer_outdir, 'recovery_summary.csv'))
+    regrowth_df = pd.DataFrame(perc_regrowth_dict)
+    regrowth_df.to_csv(os.path.join(outer_outdir, 'regrowth_summary.csv'))
+    growth_df = pd.DataFrame(growth_dict)
+    growth_df.to_csv(os.path.join(outer_outdir, 'growth_summary.csv'))
+
+max(0.02, (1 + 2.6 * flgrem - (5.83* (flgrem**2)))*agp_in))
+
+
+def erase_intermediate_files(outerdir):
+    for folder in os.listdir(outerdir):
+        try:
+            for file in os.listdir(os.path.join(outerdir, folder)):
+                if file.endswith("summary_results.csv") or \
+                file.startswith("forage-log") or \
+                file.endswith("summary.csv"):
+                    continue
+                else:
+                    try:
+                        object = os.path.join(outerdir, folder, file)
+                        if os.path.isfile(object):
+                            os.remove(object)
+                        else:
+                            shutil.rmtree(object)
+                    except OSError:
+                        continue
+        except WindowsError:
+            continue
+
 if __name__ == "__main__":
+    outerdir = r"C:\Users\Ginger\Dropbox\NatCap_backup\Forage_model\Forage_model\model_results\WitW\Ortega-S_et_al\defol_exp"
     optimal_defoliation_test()
+    erase_intermediate_files(outerdir)
